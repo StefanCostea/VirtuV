faea07bd1668acba3ba0c4b7cc9eae5694622261
StefanCostea <stefanxenadu@gmail.com>
2024-11-28 23:15:17 +0100

    FetchStage correct interface

--- src/core/cpu/pipeline/fetch/FetchStage.hpp
+++ src/core/cpu/pipeline/fetch/FetchStage.hpp
@@ -6,11 +6,11 @@
 class FetchStage : public PipelineStage {
 private:
     Memory* memory;
-    RegisterBank* cpu;
+    RegisterBank* registers;
     uint32_t fetched_instruction;
 
 public:
-    FetchStage(Memory* memory, CPU* cpu);
+    FetchStage(Memory* memory, RegisterBank* registers);
     void process() override;
     uint32_t get_fetched_instruction();
 };
