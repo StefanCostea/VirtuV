2bb9940363242dbdf0df4f27a746acf505db9087
jsec0x1b <galvez.jose.tpr3a@gmail.com>
2024-11-29 02:08:58 +0100

    test branch protection2

--- src/core/cpu/CPU.hpp
+++ src/core/cpu/CPU.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 #include "core/cpu/pipeline/Pipeline.hpp"
 #include "core/cpu/register_bank/RegisterBank.hpp"
 #include "core/memory/Memory.hpp"
--- src/core/cpu/register_bank/RegisterBank.hpp
+++ src/core/cpu/register_bank/RegisterBank.hpp
@@ -1,14 +1,14 @@
 #pragma once
-#include <array>
+//#include <array>
 //#include <cstdint>
-#include <stdexcept>
+//#include <stdexcept>
 
 class RegisterBank {
 private:
     std::array<uint32_t, 32> registers{}; // General-purpose registers
 
 public:
-    RegisterBank();
+    RegisterBang();
 
     uint32_t read(uint8_t reg);
     void write(uint8_t reg, uint32_t value);
